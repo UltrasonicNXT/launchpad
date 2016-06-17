71d18a35b7d097486b04c3a8cf7090380257030f
UltrasonicNXT <adamr_carter@btinternet.com>
2016-06-17 12:25:00 +0100

    Working class structure

--- midi.py
+++ midi.py
@@ -6,29 +6,37 @@ import random
 noteOn = 1
 noteOff = 0
 
-inputConnection = None
-outputConnection = None
+class Connection:
+    def __init__(self):
+        infos = [midi.get_device_info(n) for n in range(0, midi.get_count())]
+        i=0
+        o=0
+        for (n,info) in enumerate(infos):
+            if info[2] and "Launchpad" in info[1]:
+                i=n
+            elif info[3] and "Launchpad" in info[1]:
+                o=n
+        print "Detected Launchpad on input " + str(i) + " and output " + str(o)
+        self.input = midi.Input(i)
+        self.output = midi.Output(o)
+        print "Connection established"
 
-def begin():
-    (inputID,outputID) = deviceInfo();
-    print "Detected Launchpad on input " + str(inputID) + " and output " + str(outputID)
-    inputConnection = midi.Input(inputID)
-    outputConnection = midi.Output(outputID)
-    print "Connection established"
+    def poll(self):
+        return self.input.poll()
 
-def deviceInfo():
-    infos = [midi.get_device_info(n) for n in range(0, midi.get_count())]
-    i=0
-    o=0
-    for (n,info) in enumerate(infos):
-        if info[2] and "Launchpad" in info[1]:
-            i=n
-        elif info[3] and "Launchpad" in info[1]:
-            o=n
-    return (i,o)
+    def read(self,n=1):
+        data = self.input.read(n)
+        return Note(self,data=data)
+
+    def close(self):
+        self.input.close()
+        self.output.close()
+        midi.quit()
+
 
 class Note:
-    def __init__(self,data=None,coordinate=None,velocity=None):
+    def __init__(self,connection,data=None,coordinate=None,velocity=None):
+        self.connection=connection
         if data:
             self.velocity = data[0][0][2]
             self.number = data[0][0][1]
@@ -45,12 +53,7 @@ class Note:
 
     def send(self):
         data = [[[144, self.number, self.velocity, 0], midi.time()]]
-        outputConnection.write(data)
-
-def close():
-    inputConnection.close()
-    outputConnection.close()
-    midi.quit()
+        self.connection.output.write(data)
 
 
 
