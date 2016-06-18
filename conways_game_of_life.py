f0cd5d552fd552f37e9a47634a2296dc843bf472
UltrasonicNXT <adamr_carter@btinternet.com>
2016-06-18 11:57:31 +0100

    Working game of life

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -1,4 +1,5 @@
 import midi
+import time
 
 conn = midi.Connection()
 
@@ -20,3 +21,63 @@ while True:
             led.send()
         elif button.side:
             break
+
+while True:
+    newMatrix = [[0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0]]
+    for y,row in enumerate(matrix):
+        for x,val in enumerate(row):
+            neighbors = []
+            if x > 0:
+                neighbors.append(matrix[y][x-1])
+            if x < 7:
+                neighbors.append(matrix[y][x+1])
+            if y > 0:
+                neighbors.append(matrix[y-1][x])
+            if y < 7:
+                neighbors.append(matrix[y+1][x])
+            if x > 0 and y > 0:
+                neighbors.append(matrix[y-1][x-1])
+            if x > 0 and y < 7:
+                neighbors.append(matrix[y+1][x-1])
+            if x < 7 and y > 0:
+                neighbors.append(matrix[y-1][x+1])
+            if x < 7 and y < 7:
+                neighbors.append(matrix[y+1][x+1])
+            aliveNeighbors = 0
+            for val in neighbors:
+                if val:
+                    aliveNeighbors += 1
+            if matrix[y][x]:
+                if aliveNeighbors < 2:
+                    newMatrix[y][x] = 0
+                elif aliveNeighbors < 4:
+                    newMatrix[y][x] = 1
+                else:
+                    newMatrix[y][x] = 0
+            else:
+                if aliveNeighbors == 3:
+                    newMatrix[y][x] = 1
+
+    for y,row in enumerate(matrix):
+        for x,val in enumerate(row):
+            led = midi.LED(conn,(x,y),127*newMatrix[y][x])
+            led.send()
+    matrix = newMatrix
+    
+    time.sleep(1)
+    
+    """if conn.poll():
+        button = conn.read()
+        if button.side:
+            break"""
+
+conn.close()
+                
+        
